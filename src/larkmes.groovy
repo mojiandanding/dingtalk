69ff3165098bf290209250ae01018a37e65df528
mojiandanding <131851528+mojiandanding@users.noreply.github.com>
2024-10-29 10:25:23 +0800

    Update larkmes.groovy

--- src/larkmes.groovy
+++ src/larkmes.groovy
@@ -48,7 +48,7 @@ def GetSuccessContent()
 {
   wrap([$class: 'BuildUser']) {
     def Content = ""
-	  Content = "项目名称：**${env.JOB_NAME}**\\n当前版本：**${env.BUILD_NUMBER}**\\n构建发起：**${env.GIT_COMMITTER_NAME}**\\n持续时间：**${currentBuild.durationString}**\\n构建日志：**[点击查看详情](${env.BUILD_URL}console)**\\n构建结果：**构建成功 ✅**"
+	  Content = "项目名称：**${env.JOB_NAME}**\\n当前版本：**${env.BUILD_NUMBER}**\\n构建发起：**${env.GITLAB_USER_LOGIN}**\\n持续时间：**${currentBuild.durationString}**\\n构建日志：**[点击查看详情](${env.BUILD_URL}console)**\\n构建结果：**构建成功 ✅**"
     return Content
 	} 
 }
@@ -57,7 +57,7 @@ def GetFailureContent()
 {
   wrap([$class: 'BuildUser']) {
     def Content = ""
-	  Content = "项目名称：**${env.JOB_NAME}**\\n当前版本：**${env.BUILD_NUMBER}**\\n构建发起：**${env.GIT_COMMITTER_NAME}**\\n持续时间：**${currentBuild.durationString}**\\n构建日志：**[点击查看详情](${env.BUILD_URL}console)**\\n构建结果：**构建失败 ❌**"
+	  Content = "项目名称：**${env.JOB_NAME}**\\n当前版本：**${env.BUILD_NUMBER}**\\n构建发起：**${env.GITLAB_USER_LOGIN}**\\n持续时间：**${currentBuild.durationString}**\\n构建日志：**[点击查看详情](${env.BUILD_URL}console)**\\n构建结果：**构建失败 ❌**"
     return Content
 	} 
 }
